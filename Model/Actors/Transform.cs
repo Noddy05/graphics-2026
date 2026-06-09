cdb3476589fbc3ec0f3e630ef6c4ad39b155cfc3
Noah Dirksen <noah05dirksen@gmail.com>
2026-06-09 12:19:04 +0200

    removed 'caching' from transform

--- Model/Actors/Transform.cs
+++ Model/Actors/Transform.cs
@@ -12,20 +12,12 @@ namespace Graphics2026.Model.Actors
         private Transform? parent;
         private List<Transform> children = new List<Transform>();
 
-        private Matrix4? cachedWorldTransform;
-        private Vector3 cachedPosition;
-        private Vector3 cachedRotation;
-        private Vector3 cachedSize;
-
         private Matrix4 matrixMod = Matrix4.Identity;
         private readonly IRenderable renderable;
 
         public Transform(IRenderable renderable)
         {
             this.renderable = renderable;
-            cachedPosition = localPosition;
-            cachedRotation = localRotation;
-            cachedSize = localSize;
         }
 
         public void SetParent(Transform? parent)
@@ -54,7 +46,6 @@ namespace Graphics2026.Model.Actors
         public Transform? GetParent() => parent;
         public IRenderable GetRenderable() => renderable;
 
-        public void ClearCache() => cachedWorldTransform = null;
         public Matrix4 LocalSize() => Matrix4.CreateScale(localSize);
         public Matrix4 LocalPosition() => Matrix4.CreateTranslation(localPosition);
         public Matrix4 LocalRotation() => Matrix4.CreateFromQuaternion(
@@ -63,31 +54,6 @@ namespace Graphics2026.Model.Actors
         public Matrix4 LocalTransform() => matrixMod * LocalSize() * LocalRotation() * LocalPosition();
         public Matrix4 LocalTransformWithoutMod() => LocalSize() * LocalRotation() * LocalPosition();
 
-        /* over-engineered
-        public Matrix4 WorldTransform()
-        {
-            //Check if cached
-            if (cachedWorldTransform != null)
-            {
-                if(cachedPosition == position && cachedRotation == rotation && cachedSize == size)
-                    return (Matrix4)cachedWorldTransform;
-
-                foreach(Transform child in children)
-                    child.ClearCache();
-
-                cachedPosition = position;
-                cachedRotation = rotation;
-                cachedSize = size;
-            }
-
-            Matrix4 finalTransform = LocalTransform();
-
-            if (parent != null)
-                finalTransform *= parent.WorldTransform();
-
-            cachedWorldTransform = finalTransform;
-            return finalTransform;
-        }*/
         public Vector3 WorldPosition() => (new Vector4(0, 0, 0, 1) * WorldTransform()).Xyz;
 
         public Matrix4 WorldTransform()
@@ -106,18 +72,6 @@ namespace Graphics2026.Model.Actors
 
             return Matrix4.Identity;
         }
-
-        /*
-        public Vector3 Forward() => (WorldTransform() * new Vector4(0, 0, -1, 1) -
-            WorldTransform() * new Vector4(0, 0, 0, 1)).Xyz;
-        public Vector3 Backward() => -Forward();
-        public Vector3 Up() => (WorldTransform() * new Vector4(0, 1, 0, 1) -
-            WorldTransform() * new Vector4(0, 0, 0, 1)).Xyz;
-        public Vector3 Down() => -Up();
-        public Vector3 Right() => (WorldTransform() * new Vector4(1, 0, 0, 1) -
-            WorldTransform() * new Vector4(1, 0, 0, 1)).Xyz;
-        public Vector3 Left() => -Right();
-        */
 
         public Vector3 Forward() => OrientVector(-Vector3.UnitZ);
         public Vector3 Backward() => -Forward();
